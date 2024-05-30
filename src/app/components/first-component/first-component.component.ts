8cca99329c142165082b133678fdc08fea11814b
Alisson Wenceslau <alisson.wenceslau@gmail.com>
2024-05-30 16:13:15 -0300

    Interpoolação nos componentes

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
-
+  name: string = "Alisson";
+  age: number = 29;
+  job = "Programador"
+  hobbies = ["Correr", "Brincar", "Estudar"]
+  car = {
+    name: "Tesla",
+    year: "2022"
+  }
   constructor() { }
 
   ngOnInit(): void {
